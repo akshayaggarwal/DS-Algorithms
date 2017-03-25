7cbeb0157bdc436f44a0d9d9f06415fbac4329b1
Akshay <akadelhi1994@gmail.com>
2017-03-25 14:03:48 -0700

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -4,48 +4,35 @@ public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int i,j,k;
         List<List<Integer>> m1 = new ArrayList<List<Integer>>();
-        
-        //int data[] = new int[3];
-        
+        Arrays.sort(nums);
+
         for(i=0;i<(nums.length-2);i++)
         {
-            for(j=i+1;j<(nums.length-1);j++)
+            j=i+1;
+            k=nums.length - 1;
+            
+            while(true)
             {
-                for(k=j+1;k<nums.length;k++)
+                if((nums[i]+nums[j]+nums[k]) == 0)
                 {
-                    if((nums[i]+nums[j]+nums[k]) == 0)
-                    {
-                              
-                        //Set<Integer> set = new TreeSet<Integer>();
-                        List<Integer> m2 = new ArrayList<Integer>();
-
-                        //data[0] = nums[i];
-                        //data[1] = nums[j];
-                        //data[2] = nums[k];
-                        
-                        m2.add(nums[i]);
-                        m2.add(nums[j]);
-                        m2.add(nums[k]);
-                        
-                        Collections.sort(m2);
-                        
-                        //set.add(nums[i]);
-                        //set.add(nums[j]);
-                        //set.add(nums[k]);
-                        
-                       // System.out.println(m2);
-                        
-                        
-                    
-                        if(!m1.contains(m2))    
-                            m1.add(m2);
-                        
-                        //m2.removeAll(m2);
-                        //m1.removeAll(m1);
-                    }
+                        if(!m1.contains((Arrays.asList(nums[i], nums[j], nums[k]))))    
+                            m1.add((Arrays.asList(nums[i], nums[j], nums[k])));
+                        j++;
+                        k--;
                 }
+                
+                else if((nums[j]+nums[k]) < (-nums[i]))
+                    j++;
+                    
+                else
+                    k--;
+                    
+                if(j>=k)
+                    break;
             }
         }
-        return m1;
+     return m1;   
     }
 }
+
+        
