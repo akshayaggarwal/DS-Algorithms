4ec67d91cc24619a33b2ded38df884872a2605cf
Akshay <akadelhi1994@gmail.com>
2017-03-25 13:14:18 -0700

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -5,7 +5,7 @@ public class Solution {
         int i,j,k;
         List<List<Integer>> m1 = new ArrayList<List<Integer>>();
         
-        int data[] = new int[3];
+        //int data[] = new int[3];
         
         for(i=0;i<(nums.length-2);i++)
         {
@@ -15,17 +15,30 @@ public class Solution {
                 {
                     if((nums[i]+nums[j]+nums[k]) == 0)
                     {
+                              
+                        //Set<Integer> set = new TreeSet<Integer>();
                         List<Integer> m2 = new ArrayList<Integer>();
 
-                        data[0] = nums[i];
-                        data[1] = nums[j];
-                        data[2] = nums[k];
+                        //data[0] = nums[i];
+                        //data[1] = nums[j];
+                        //data[2] = nums[k];
                         
                         m2.add(nums[i]);
                         m2.add(nums[j]);
                         m2.add(nums[k]);
                         
-                        m1.add(m2);
+                        Collections.sort(m2);
+                        
+                        //set.add(nums[i]);
+                        //set.add(nums[j]);
+                        //set.add(nums[k]);
+                        
+                       // System.out.println(m2);
+                        
+                        
+                    
+                        if(!m1.contains(m2))    
+                            m1.add(m2);
                         
                         //m2.removeAll(m2);
                         //m1.removeAll(m1);
